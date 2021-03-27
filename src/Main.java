57645be02bb1fe0061432e20848e9eddd8e4b147
Luis Talhado <38620948+Loco-c@users.noreply.github.com>
2021-03-27 23:04:55 +0000

    Added program
    Add the very simple program

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int [] marks = new int[6];
+        float total, avg;
+        total = 0;
+        Scanner scan = new Scanner(System.in);
+        for( int i =0; i < 6; i++){
+            System.out.println("Enter results: " +(i+1) +":");
+            marks[i] = scan.nextInt();
+            total= total + marks[i];
+        }
+        scan.close();
+
+        avg= total/6;
+        System.out.println("Student end of year result is: ");
+        if(avg>70){
+            System.out.println('A');
+        } else if (avg>=60&&  avg<=70){
+            System.out.println('B');
+        } else if (avg>= 40 && avg>=55){
+            System.out.println('C');
+        } else {
+            System.out.println('D');
+        }
+    }
+}
